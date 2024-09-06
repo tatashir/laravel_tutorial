e01aaa85db03fa34c4257ad389d8e4fb719ccd90
tatashir <115047593+tatashir@users.noreply.github.com>
2024-09-06 16:33:03 +0900

    ログインしてる人だけが`/myCart`にアクセスできる

--- ec-app/routes/web.php
+++ ec-app/routes/web.php
@@ -20,13 +20,13 @@ use App\Http\Controllers\StockController;
 // });
 Route::get('/', [StockController::class, 'index'])->name('stock.index');
 
-Route::get('/myCart', [StockController::class, 'myCart'])->name('stock.myCart');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/myCart', [StockController::class, 'myCart'])->name('stock.myCart');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
