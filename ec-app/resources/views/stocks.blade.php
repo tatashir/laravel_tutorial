13001fc6dc0ce962f5369b11e8652bde94465cee
tatashir <115047593+tatashir@users.noreply.github.com>
2024-09-06 16:55:40 +0900

    ボタンの装飾を追加

--- ec-app/resources/views/stocks.blade.php
+++ ec-app/resources/views/stocks.blade.php
@@ -4,32 +4,32 @@
             <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
             <div class="">
                 <div class="grid grid-cols-4 gap-4 flex-wrap">
-                    {{-- 追加 --}}
                     @foreach($stocks as $stock)
-                    <div class="mycart_box text-center rounded shadow-lg bg-white p-6">
+                    {{-- text-smを追加 --}}
+                    <div class="mycart_box text-sm text-center rounded shadow-lg bg-white p-6">
                         {{$stock->name}} <br>
                         {{$stock->fee}}円<br>
                         <img src="/image/{{$stock->imagePath}}" alt="" class="incart">
                         <br>
                         {{$stock->explain}} <br>
 
-                        {{-- 追加 --}}
+
                         <form action="addMyCart" method="post">
                             @csrf
                             <input type="hidden" name="stockId" value="{{ $stock->id }}">
-                            <input type="submit" value="カートに入れる">
+                            {{-- 変更 --}}
+                            <button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded ">カートに入れる</button>
                         </form>
-                        {{-- ここまで --}}
+
                     </div>
 
                     @endforeach
-                    {{-- ここまで --}}
+
                 </div>
-                {{-- 追加 --}}
                 <div class="text-center" style="width: 200px;margin: 20px auto;">
                     {{ $stocks->links()}}
                 </div>
-                {{-- ここまで --}}
+
             </div>
         </div>
     </div>
