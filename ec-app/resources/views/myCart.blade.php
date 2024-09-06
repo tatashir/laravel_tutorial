c73fbafc4343f2283d8730bd64ba545d1e0d1d2e
tatashir <115047593+tatashir@users.noreply.github.com>
2024-09-06 17:03:54 +0900

    Refactor UserStock model and myCart view

--- ec-app/resources/views/myCart.blade.php
+++ ec-app/resources/views/myCart.blade.php
@@ -1,16 +1,27 @@
 <x-app-layout>
     <div class="container-fluid">
         <div class="mx-auto" style="max-width:1200px">
-            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
+            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">
+                {{ Auth::user()->name }}さんのカートの中身
+            </h1>
+            <p class="text-center">{{ $message ?? '' }}</p><br>
             <div class="">
-                <div class="">
-                    {{-- 追加 --}}
-                    @foreach($myCartStocks as $stock)
-                    {{$stock->stockId}}<br>
-                    {{$stock->userId}}<br>
-                    @endforeach
-                    {{-- ここまで --}}
+                {{-- 追加 --}}
+                @foreach($myCartStocks as $stock)
+                <div class="text-center rounded shadow-lg bg-white p-6 m-4">
+                    {{$stock->stock->name}} <br>
+                    {{ number_format($stock->stock->fee)}}円 <br>
+                    <div class="incart flex justify-center p-4 m-4">
+                        <img src="/image/{{$stock->stock->imagePath}}" alt="" width="600">
+                    </div>
+                    <form action="/deleteMyCartStock" method="post">
+                        @csrf
+                        <input type="hidden" name="stockId" value="{{ $stock->stock->id }}">
+                        <input type="submit" value="カートから削除する">
+                    </form>
                 </div>
+                @endforeach
+                {{-- ここまで --}}
             </div>
         </div>
     </div>
