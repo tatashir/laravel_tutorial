4fc8fe6a58b8a333bd5e0f19c64975abc480308b
tatashir <115047593+tatashir@users.noreply.github.com>
2024-09-06 16:46:34 +0900

    add: `addMyCart()`

--- ec-app/app/Http/Controllers/StockController.php
+++ ec-app/app/Http/Controllers/StockController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Stock;
 use App\Models\UserStock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StockController extends Controller
 {
@@ -18,5 +19,23 @@ class StockController extends Controller
     {
         $myCartStocks = UserStock::all();
         return view('myCart', compact('myCartStocks'));
+    }
+
+    public function addMyCart(Request $request)
+    {
+        $userId = Auth::id();
+        $stockId = $request->input('stockId');
+
+        $cartAddInfo = UserStock::firstOrCreate(['stockId' => $stockId, 'userId' => $userId]);
+
+        if ($cartAddInfo->wasRecentlyCreated) {
+            $message = 'カートに追加しました';
+        } else {
+            $message = 'カートに登録済みです';
+        }
+
+        $myCartStocks = UserStock::where('userId', $userId)->get();
+
+        return view('myCart', compact('myCartStocks', 'message'));
     }
 }
