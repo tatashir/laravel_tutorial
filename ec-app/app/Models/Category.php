35f1da9d13e5d3d741897823ec4f10b75122a4b6
tatashir <115047593+tatashir@users.noreply.github.com>
2024-09-09 13:01:13 +0900

    Add migration for creating categories table

---
+++ ec-app/app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Relation segments.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
+}
